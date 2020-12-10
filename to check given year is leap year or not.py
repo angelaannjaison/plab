b3146f2c3b8fd1b6f94f4be73ede420c8ef359f7
ANGELA ANN JAISON <74919017+angelaannjaison@users.noreply.github.com>
2020-12-10 10:13:01 +0530

    Update to check given year is leap year or not.py

--- to check given year is leap year or not.py
+++ to check given year is leap year or not.py
@@ -1,6 +1,6 @@
 print("To check the given year is leap year or not ")
 y=int(input("Enter a year = "))
-if y%400 == 0 or (y%100 != 100 and y%4 ==0):
+if y%400 == 0 or (y%100 != 0 and y%4 ==0):
     print(y,"is a leap year")
 else:
     print(y," is not a yeap year")
